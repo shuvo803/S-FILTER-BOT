0e7235a59053cd480cf0a38420c14abbed2cabe9
shuvo803 <mdabulkhair675@gmail.com>
2025-10-10 12:05:25 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,7 +75,7 @@ async def start(client, message):
             ]]
         if CLONE_MODE == True:
             
-        reply_markup = InlineKeyboardMarkup(buttons)
+        
         m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
         await asyncio.sleep(1)
         await m.delete()
@@ -161,7 +161,7 @@ async def start(client, message):
             ]]
         if CLONE_MODE == True:
             
-        reply_markup = InlineKeyboardMarkup(buttons)      
+              
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -216,7 +216,7 @@ async def start(client, message):
                 ]]
             if CLONE_MODE == True:
                 
-            reply_markup = InlineKeyboardMarkup(buttons)
+            
             m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
             await asyncio.sleep(1)
             await m.delete()
@@ -1404,6 +1404,7 @@ async def purge_requests(client, message):
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
         )
+
 
 
 
